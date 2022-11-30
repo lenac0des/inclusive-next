35f12e913a3076b39a85bfb8e8179771a924b6ef
lenac0des <ellenajean@gmail.com>
2022-11-30 14:39:49 -0500

    footer styling

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,19 +1,54 @@
+import React from 'react'
+import { FooterStyled, Logo, IconText, Row, Column, Caption, Caption2, Icon } from './styles/Footer.js'
+import { TwitterSVG, FacebookSvg, InstagramSvg, YoutubeSvg } from '../../assets/Icons/index.js'
 
-import styled from "styled-components";
-
-const FooterPrivisional = styled.div`
-  height: 100vh;
-  border: 3px cornflowerblue solid;
-  display:grid;
-  place-items: center;
-`
-
-const Footer = () => {
+export default function Footer() {
   return (
-    <FooterPrivisional>
-      Footer Component
-    </FooterPrivisional>
+    <FooterStyled>
+
+      <Logo>
+        Inclusive
+      </Logo>
+      <IconText>
+        Connect with us!
+      </IconText>
+      <Icon>
+        <TwitterSVG />
+        <InstagramSvg />
+        <FacebookSvg />
+        <YoutubeSvg />
+      </Icon>
+
+
+      <Row>
+        <Column>
+          <h4>Learn</h4>
+          <h5>About us</h5>
+          <h5>Blog</h5>
+          <h5>Newsletter</h5>
+        </Column>
+
+
+        <Column>
+          <h4>Inclusive Services</h4>
+          <h5>Guide to services</h5>
+          <h5>Resource directory</h5>
+        </Column>
+
+        <Column>
+          <h4>Help</h4>
+          <h5>Contact us</h5>
+          <h5>FAQ</h5>
+        </Column>
+
+      </Row>
+
+      <Caption>
+        © 2022 The Inclusive Group, All rights reserved
+      </Caption>
+      <Caption2>Nonprofit helping NYC parents find the best services for their children.</Caption2>
+
+    </FooterStyled>
+
   );
 }
-
-export default Footer;
---
+++ components/footer/styles/Footer.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+ 
+
+export const FooterStyled = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 22px 0px 6px;
+gap: 16px;
+width: 1550px;
+height: 450px;
+left: 0px;
+top: 2804px;
+background: #F4F4F4;
+` 
+export const Logo = styled.h2`
+margin-left: -50px;
+margin-top: -25px;
+margin-right: 850px;
+font-size: 34px;
+line-height: -55px;
+font-famliy: 'Poppins';
+font-weight: 800;
+color: rgba(164, 97, 187, 1)
+`
+
+export const IconText = styled.h4`
+border: 2px solid red;
+font-size: 20px;
+letter-spacing: 0.05em;
+font-style: normal;
+line-height: 32px;
+font-family: 'Poppins';
+font-weight: 600;
+color: #00080B;
+`
+export const Icon = styled.i`
+width: 36px;
+height: 36px;
+`
+
+
+export const Row = styled.div`
+border: 2px solid green;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+grid-gap: 60px;
+justify-content: center;
+
+
+@media (max-width: 1000px) {
+   grid-template-columns: repeat()(auto-fill, minmax()(200px,1fr));
+   grid-gapL 20px;
+}
+`
+
+export const Column = styled.div`
+border: 3px solid orange;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 22px 0px 6px;
+gap: 16px;
+line-height: 32px;
+`
+export const Caption = styled.h6`
+border: 3px dotted pink;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+letter-spacing: 0.01em;
+line-height: 35px;
+margin-left: -1050px;
+margin-bottom: -50px;
+`
+
+export const Caption2 = styled.h6`
+border:5px dotted blue;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+letter-spacing: 0.01em;
+line-height: 24px;
+margin-right: -880px;
+`
